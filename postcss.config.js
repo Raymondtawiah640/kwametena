2a9f997c8f6f5d9cf9440b5efdad5271f62074fc
Raymondtawiah640 <enterprisekiln@gmail.com>
2025-09-11 10:04:28 -0900

    modified

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 // postcss.config.js
-module.exports = {
-=======
 export default {
->>>>>>> 6edaa4f (Initial commit)
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-<<<<<<< HEAD
-};
-=======
 }
->>>>>>> 6edaa4f (Initial commit)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Ensure Tailwind purges unused styles
-=======
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
->>>>>>> 6edaa4f (Initial commit)
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
-};
-=======
 }
->>>>>>> 6edaa4f (Initial commit)
